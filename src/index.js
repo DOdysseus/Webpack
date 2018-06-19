70c2667e57b97f8aa409f9007cf2f292b9a1b829
Dhanasekar Murugesan <dhanasekar.murugesan@Aspiresys.com>
2018-06-19 17:42:06 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//imports the Sum from file.
+const sum = require('./sum');
+
+const total = sum(44,45);
+
+console.log(total);
---
+++ src/sum.js
@@ -0,0 +1,6 @@
+const sum = (a,b) => a + b;
+
+
+//this exports whatever is assigned and make it available 
+//whoever import it.
+module.exports = sum;
